5fe2f67cdbf795a7b7bfaaa17754e89418726715
Your Name <your.email@example.com>
2025-10-31 10:32:49 +0100

    filtrer_produits_expires

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ import { PRODUCTS } from "./data/products.js";
 PRODUCTS.forEach((p) => console.log(p));
 
 PRODUCTS.forEach((p) => console.log(p.name));
+
+//etape4
+import { isExpired } from "./utils/date.js";
+const expired = PRODUCTS.filter(p => isExpired(p.expiryDate));
+console.log("Produits expirés :", expired);
+
